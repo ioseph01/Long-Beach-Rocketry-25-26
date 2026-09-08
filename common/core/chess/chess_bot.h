bd2893302180b63246afa8a364f2afc97ebc503d
ioseph01 <j0ni0n.clang@gmail.com>
2026-09-07 21:33:33 -0700

    Current progress

---
+++ common/core/chess/chess_bot.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdint>
+#include "chess_globals.h"
+
+namespace LBR
+{
+namespace Chess
+{
+
+class ChessBot
+{
+};
+
+}  // namespace Chess
+}  // namespace LBR
---
+++ common/core/chess/chess_moves.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "chess_board.h"
+#include "chess_globals.h"
+
+namespace LBR
+{
+namespace Chess
+{
+uint8_t get_player_moves(ChessBoard& board);
+}
+}  // namespace LBR
